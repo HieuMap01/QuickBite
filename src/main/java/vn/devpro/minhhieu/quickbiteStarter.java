b5e0482e5b2629fd7ea8491526a8f95bc3f68218
HieuMap01 <buiminhhieu12092003@gmail.com>
2025-10-05 02:43:52 +0700

    Product save-add

--- src/main/java/vn/devpro/minhhieu/quickbiteStarter.java
+++ src/main/java/vn/devpro/minhhieu/quickbiteStarter.java
@@ -2,13 +2,17 @@ package vn.devpro.minhhieu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-secret.properties") // nạp thêm (import) một file cấu hình .properties từ
+															// classpath vào Environment (môi trường cấu hình của ứng
+															// dụng).
 public class quickbiteStarter {
 
-    public static void main(String[] args) {
-        
-        SpringApplication Start = new SpringApplication(quickbiteStarter.class);
-        Start.run(args);
-    }
+	public static void main(String[] args) {
+
+		SpringApplication Start = new SpringApplication(quickbiteStarter.class);
+		Start.run(args);
+	}
 }
